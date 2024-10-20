7d0cc12041c84d30f01f5d053caea881228e2353
Mr-Maksim <55948966+mr-maksim@users.noreply.github.com>
2024-10-20 12:54:02 +0300

    Создано базовое окно приложения
    Changes to be committed:
    	modified:   main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+
+# Настройка главного окна
+win = tk.Tk()
+win.title('RS-485 Scanner')
+win.geometry('715x750')
+
+
+
+
+# Запуск приложения
+win.mainloop()
